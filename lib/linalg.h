fbe20bedce44b01c2b0eb95f852367b2e2b9e040
Joshua Pinto <joshuapinto470@gmail.com>
2021-05-28 11:52:43 +0530

    #linalg.h Added template support. Added typedef for Mat44, Vec3, Vec2 classes.
    #projection.cpp
    Changed default wavefront file location.

--- lib/linalg.h
+++ lib/linalg.h
@@ -1,60 +1,58 @@
-#ifndef LINALG_H
-#define LINALG_H
+#pragma once
 
 #define PI 3.14159265359
 
 #include<iostream>
 #include<math.h>
 
-class Mat44f {
+
+template <typename T>
+class Mat44 {
 private:
-	float data[4][4] = { {1,0,0,0},
-						 {0,1,0,0},
-						 {0,0,1,0},
-						 {0,0,0,1} };
+	T data[4][4] = { {1,0,0,0},
+					 {0,1,0,0},
+					 {0,0,1,0},
+					 {0,0,0,1}};
 
 public:
-	Mat44f(float [][4]);
-	Mat44f(void);
-	float det();
-	Mat44f inverse();
-	float* operator[](int i) { return data[i]; }
-	Mat44f operator* (float);
-	Mat44f operator* (Mat44f);
-	Mat44f& zeros();
-	Mat44f& ones();
+	Mat44(T[][4]);
+	Mat44(void);
+	T det();
+	Mat44 inverse();
+	T* operator[](int i) { return data[i]; }
+	Mat44 operator* (T);
+	Mat44 operator* (Mat44);
+	Mat44& zeros();
+	Mat44& ones();
 };
 
-Mat44f::Mat44f(float d[][4]) {
+template <typename T>
+Mat44<T>::Mat44(T d[][4]) {
 	for (int i = 0; i < 4;i++) 
 		for (int j = 0;j < 4;j++)
 			data[i][j] = d[i][j];
 }
-Mat44f::Mat44f(void) {
+template <typename T>
+Mat44<T>::Mat44(void) {
 	for (int i = 0;i < 4;i++)
 		data[i][i] = 1;
 }
 
-Mat44f Mat44f :: operator*(float scale) {
-	float x[4][4];
+template <typename T>
+Mat44<T> Mat44<T> :: operator*(T scale) {
+	T x[4][4];
 	for (int i = 0;i < 4;i++) {
 		for (int j = 0;j < 4;j++) {
 			x[i][j] = data[i][j] * scale;
 		}
 	}
-	Mat44f mat(x);
+	Mat44 mat(x);
 	return mat;
 }
 
-/*
-00 01 02 03
-10 11 12 13
-20 21 22 23
-30 31 32 33
-*/
-
-Mat44f Mat44f :: operator*(Mat44f M) {
-	Mat44f mul;
+template <typename T>
+Mat44<T> Mat44<T> :: operator*(Mat44<T> M) {
+	Mat44 mul;
 	int i, j, k;
 	for (int i = 0;i < 4;i++) {
 		for (int j = 0;j < 4;j++) {
@@ -69,35 +67,36 @@ Mat44f Mat44f :: operator*(Mat44f M) {
 	return mul;
 }
 
-float Mat44f::det() {
-	float m1 = data[1][1] * (data[2][2] * data[3][3] - data[2][3] * data[3][2]) -
+template <typename T>
+T Mat44<T>::det() {
+	T m1 = data[1][1] * (data[2][2] * data[3][3] - data[2][3] * data[3][2]) -
 		data[1][2] * (data[2][1] * data[3][3] - data[2][3] * data[3][1]) +
 		data[1][3] * (data[2][1] * data[3][2] - data[2][2] * data[3][1]);
 	
-	float m2 = data[1][0] * (data[2][2] * data[3][3] - data[2][3] * data[3][2]) -
+	T m2 = data[1][0] * (data[2][2] * data[3][3] - data[2][3] * data[3][2]) -
 		data[1][2] * (data[2][0] * data[3][3] - data[2][3] * data[3][0]) +
 		data[1][3] * (data[2][0] * data[3][2] - data[2][2] * data[3][0]);
 
-	float m3 = data[1][0] * (data[2][1] * data[3][3] - data[2][3] * data[3][1]) -
+	T m3 = data[1][0] * (data[2][1] * data[3][3] - data[2][3] * data[3][1]) -
 		data[1][1] * (data[2][0] * data[3][3] - data[2][3] * data[3][0]) +
 		data[1][3] * (data[2][0] * data[3][1] - data[2][1] * data[3][0]);
 
-	float m4 = data[1][0] * (data[2][1] * data[3][2] - data[2][2] * data[3][1]) -
+	T m4 = data[1][0] * (data[2][1] * data[3][2] - data[2][2] * data[3][1]) -
 		data[1][1] * (data[2][0] * data[3][2] - data[2][2] * data[3][0]) +
 		data[1][2] * (data[2][0] * data[3][1] - data[2][1] * data[3][0]);
 
 	// std :: cout << m1 << " " << m2 << " " << m3 << " " << m4 << "\n";
 
-	float detm = (data[0][0] * m1) - (data[0][1] * m2) + (data[0][2] * m3) - (data[0][3] * m4);
+	T detm = (data[0][0] * m1) - (data[0][1] * m2) + (data[0][2] * m3) - (data[0][3] * m4);
 	return detm;
 
 }
+template <typename T>
+Mat44<T> Mat44<T>::inverse() {
+	T adj[4][4];
+	T M[3][3];
 
-Mat44f Mat44f::inverse() {
-	float adj[4][4];
-	float M[3][3];
-
-	float d = det();
+	T d = det();
 	if (d == 0)
 		return *this;
 	
@@ -119,25 +118,21 @@ Mat44f Mat44f::inverse() {
 					x++;
 				}
 			}
-			/*
-			00 01 02
-			10 11 12
-			20 21 22
-			*/
 
-			float det_sub = M[0][0] * (M[1][1] * M[2][2] - M[1][2] * M[2][1]) -
+			T det_sub = M[0][0] * (M[1][1] * M[2][2] - M[1][2] * M[2][1]) -
 				M[0][1] * (M[1][0] * M[2][2] - M[1][2] * M[2][0]) +
 				M[0][2] * (M[1][0] * M[2][1] - M[1][1] * M[2][0]);
 
-			adj[j][i] = (float) pow(-1, i + j) * det_sub;
+			adj[j][i] = (T) pow(-1, i + j) * det_sub;
 		}
 	}
 
-	Mat44f mat(adj);
+	Mat44 mat(adj);
 	return mat * d;
 }
 
-Mat44f& Mat44f::zeros(){
+template <typename T>
+Mat44<T>& Mat44<T>::zeros(){
 	for (int i = 0; i < 4; i++) 
 		for (int j = 0; j < 4;j++)
 			data[i][j] = 0;
@@ -145,7 +140,8 @@ Mat44f& Mat44f::zeros(){
 	return *this;
 }
 
-Mat44f& Mat44f::ones() {
+template <typename T>
+Mat44<T>& Mat44<T>::ones() {
 	for (int i = 0; i < 4; i++)
 		for (int j = 0; j < 4;j++)
 			data[i][j] = 1;
@@ -153,73 +149,87 @@ Mat44f& Mat44f::ones() {
 }
 
 // Vector 3 Class Declaration
-class Vec3f {
+template <typename T>
+class Vec3 {
 private:
-	float point[3] = { 0,0,0 };
+	T point[3] = { 0,0,0 };
 
 public:
-	Vec3f(float, float, float);
-	Vec3f() { point[0] = 0; };
+	Vec3(T, T, T);
+	Vec3() { point[0] = 0; };
 
-	float operator[](int i) const { return point[i]; }
-	float& operator[](int i) { return point[i]; }
-	float dot(Vec3f&);
-	Vec3f operator +(Vec3f);
-	Vec3f operator -(Vec3f);
-	Vec3f operator *(Vec3f);
-	Vec3f operator *(Mat44f);
-	float length_sqr(void);
+	T operator[](int i) const { return point[i]; }
+	T& operator[](int i) { return point[i]; }
+	T dot(Vec3&);
+	Vec3<T> operator +(Vec3<T>);
+	Vec3<T> operator -(Vec3<T>);
+	Vec3<T> operator *(Vec3<T>);
+	Vec3<T> operator *(Mat44<T>);
+	T length_sqr(void);
 
-	float x() const { return point[0]; };
-	float y() const { return point[1]; };
-	float z() const { return point[2]; };
+	T x() const { return point[0]; };
+	T y() const { return point[1]; };
+	T z() const { return point[2]; };
 
 };
 
-Vec3f::Vec3f(float x, float y, float z) {
+template <typename T>
+Vec3<T>::Vec3(T x, T y, T z) {
 	point[0] = x;
 	point[1] = y;
 	point[2] = z;
 }
 
-float Vec3f :: dot(Vec3f& v)
+template <typename T>
+T Vec3<T> :: dot(Vec3<T>& v)
 {
 	return v[0] + point[0] + v[1] * point[1] + v[2] * point[2];
 }
-float Vec3f :: length_sqr() {
+template <typename T>
+T Vec3<T> :: length_sqr() {
 	return point[0] * point[0] + point[1] * point[1] + point[2] * point[2];
 }
 
-Vec3f Vec3f :: operator+(Vec3f v)
+template <typename T>
+Vec3<T> Vec3<T> :: operator+(Vec3 v)
 {
-	Vec3f V(v[0] + point[0], v[1] + point[1], v[2] + point[2]);
-	return V;
-}
-Vec3f Vec3f :: operator-(Vec3f v)
-{
-	Vec3f V(v[0] - point[0], v[1] - point[1], v[2] - point[2]);
+	Vec3 V(v[0] + point[0], v[1] + point[1], v[2] + point[2]);
 	return V;
 }
 
-Vec3f Vec3f :: operator*(Mat44f M) {
-	float x = point[0] * M[0][0] + point[1] * M[1][0] + point[2] * M[2][0] + M[3][0];
-	float y = point[0] * M[0][1] + point[1] * M[1][1] + point[2] * M[2][1] + M[3][1];
-	float z = point[0] * M[0][2] + point[1] * M[1][2] + point[2] * M[2][2] + M[3][2];
-	Vec3f v(x, y, z);
+template <typename T>
+Vec3<T> Vec3<T> :: operator-(Vec3 v)
+{
+	Vec3 V(v[0] - point[0], v[1] - point[1], v[2] - point[2]);
+	return V;
+}
+
+template <typename T>
+Vec3<T> Vec3<T> :: operator*(Mat44<T> M) {
+	T x = point[0] * M[0][0] + point[1] * M[1][0] + point[2] * M[2][0] + M[3][0];
+	T y = point[0] * M[0][1] + point[1] * M[1][1] + point[2] * M[2][1] + M[3][1];
+	T z = point[0] * M[0][2] + point[1] * M[1][2] + point[2] * M[2][2] + M[3][2];
+	Vec3 v(x, y, z);
 	return v;
 
 }
-Vec3f Vec3f :: operator*(Vec3f v)
+
+
+template <typename T>
+Vec3<T> Vec3<T> :: operator*(Vec3 v)
 {
-	Vec3f V(v[0] * point[0], v[1] * point[1], v[2] * point[2]);
+	Vec3 V(v[0] * point[0], v[1] * point[1], v[2] * point[2]);
 	return V;
 }
 
-std::ostream& operator<<(std::ostream& os, const Vec3f& obj)
+template <typename T>
+std::ostream& operator<<(std::ostream& os, const Vec3<T>& obj)
 {
 	os << "<" << obj.x() << ", " << obj.y() << ", " << obj.z() << ">";
 	return os;
 }
+
+
 /*
 std::ostream& operator<<(std::ostream& os, const Vec2f& obj)
 {
@@ -231,61 +241,78 @@ std::ostream& operator<<(std::ostream& os, const Vec2f& obj)
 
 // Vector 2 
 
-class Vec2f {
-	float point[2] = { 0,0};
+template <typename T>
+class Vec2 {
+	T point[2] = { 0,0};
 
 public:
-	Vec2f(float, float);
-	Vec2f() { point[0] = 0; };
+	Vec2(T, T);
+	Vec2() { point[0] = 0; };
 
-	float operator[](int i) const { return point[i]; }
-	float& operator[](int i) { return point[i]; }
-	float dot(Vec2f&);
-	Vec2f operator +(Vec2f);
-	Vec2f operator -(Vec2f);
-	Vec2f operator *(Vec2f);
-	Vec2f operator /(float);
-	float length_sqr(void);
+	T operator[](int i) const { return point[i]; }
+	T& operator[](int i) { return point[i]; }
+	T dot(Vec2<T>&);
+	Vec2<T> operator +(Vec2<T>);
+	Vec2<T> operator -(Vec2<T>);
+	Vec2<T> operator *(Vec2<T>);
+	Vec2<T> operator /(T);
+	T length_sqr(void);
 
-	float x() const { return point[0]; };
-	float y() const { return point[1]; };
+	T x() const { return point[0]; };
+	T y() const { return point[1]; };
 };
 
-
-Vec2f::Vec2f(float x, float y) {
+template <typename T>
+Vec2<T>::Vec2(T x, T y) {
 	point[0] = x;
 	point[1] = y;
 }
-
-float Vec2f::dot(Vec2f& v)
+template <typename T>
+T Vec2<T>::dot(Vec2<T>& v)
 {
 	return v[0] + point[0] + v[1] * point[1];
 }
-float Vec2f::length_sqr() {
+
+template <typename T>
+T Vec2<T>::length_sqr() {
 	return point[0] * point[0] + point[1] * point[1];
 }
 
-Vec2f Vec2f :: operator+(Vec2f v)
+template <typename T>
+Vec2<T> Vec2<T> :: operator+(Vec2<T> v)
 {
-	Vec2f V(v[0] + point[0], v[1] + point[1]);
+	Vec2<T> V(v[0] + point[0], v[1] + point[1]);
 	return V;
 }
-Vec2f Vec2f :: operator-(Vec2f v)
+
+template <typename T>
+Vec2<T> Vec2<T> :: operator-(Vec2<T> v)
 {
-	Vec2f V(v[0] - point[0], v[1] - point[1]);
+	Vec2<T> V(v[0] - point[0], v[1] - point[1]);
 	return V;
 }
-Vec2f Vec2f :: operator*(Vec2f v)
+
+template <typename T>
+Vec2<T> Vec2<T> :: operator*(Vec2<T> v)
 {
-	Vec2f V(v[0] * point[0], v[1] * point[1]);
+	Vec2<T> V(v[0] * point[0], v[1] * point[1]);
 	return V;
 }
-Vec2f Vec2f :: operator/(float v) {
-	Vec2f p;
+
+template <typename T>
+Vec2<T> Vec2<T> :: operator/(T v) {
+	Vec2<T> p;
 	p[0] = point[0] / v;
 	p[1] = point[1] / v;
 	return p;
 }
 // General function Definations
 
-#endif 
+typedef Mat44<float> Mat44f;
+typedef Mat44<int> Mat44i;
+
+typedef Vec3<float> Vec3f;
+typedef Vec3<int> Vec3i;
+
+typedef Vec2<float> Vec2f;
+typedef Vec2<int> Vec2i;
